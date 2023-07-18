aba3d224f33ab342d419d262ff6a2918f1109f5c
gennadiy-gaysha <gennadiy-gaysha@gmail.com>
2023-07-18 18:37:00 +0200

    Create basic Student class

---
+++ student.py
@@ -0,0 +1,22 @@
+from datetime import date, timedelta
+
+
+class Student:
+    '''A Student class as base for method testing'''
+
+    def __init__(self, first_name, last_name):
+        self._first_name = first_name
+        self._last_name = last_name
+        self._start_date = date.today()
+        self._end_date = date.today() + timedelta(days=365)
+        self._naughty_list = False
+
+# The @property decorator is used in Python to define a method as a property. It allows you to access the method like an attribute, without using parentheses to invoke it as a function.
+# We use this decorator for the to get data-only methods
+    @property
+    def full_name(self):
+        return f"{self._first_name} {self._last_name}"
+
+
+student = Student("John", "Makler")
+print(student._start_date, student.full_name, student._end_date)
