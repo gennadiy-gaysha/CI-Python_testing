d2bec6c13ec2f1e7eebd68fc27f12f33e889137f
gennadiy-gaysha <gennadiy-gaysha@gmail.com>
2023-07-19 13:52:06 +0200

    Create test for apply_extension method and the method itself

--- student.py
+++ student.py
@@ -8,7 +8,7 @@ class Student:
         self._first_name = first_name
         self._last_name = last_name
         self._start_date = date.today()
-        self._end_date = date.today() + timedelta(days=365)
+        self.end_date = date.today() + timedelta(days=365)
         self._naughty_list = False
 
 # The @property decorator is used in Python to define a method as a property. It allows you to access the method like an attribute, without using parentheses to invoke it as a function.
@@ -23,3 +23,6 @@ class Student:
     @property
     def email(self):
         return (f"{self._first_name.lower()}.{self._last_name.lower()}@email.com")
+
+    def apply_extension(self, days):
+        self.end_date = self.end_date + timedelta(days=days)
